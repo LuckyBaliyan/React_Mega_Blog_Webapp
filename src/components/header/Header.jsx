e509254aab3c241eb57d29b184976f199e6674c5
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-08-02 15:34:19 +0530

    Push2

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+      <header className="flex items-center justify-center w-full">
+        <h2>Header</h2>
+      </header>
+    )
+}
+
+export default Header;
